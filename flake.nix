d5c4a954fc13d1d2f7288534d2c3a4d8144d4975
Elias Ainsworth <pilum-murialis.toge@proton.me>
2025-05-20 12:21:14 -0700

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+
+      perSystem =
+        { system, pkgs, ... }:
+        {
+          packages.default = pkgs.stdenv.mkDerivation {
+            name = "website";
+            src = ./site;
+            installPhase = ''
+              mkdir -p $out
+              cp -r * $out/
+            '';
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              curl
+              statix
+              nixfmt-rfc-style
+            ];
+          };
+        };
+
+      flake.nixosModules.default = import ./nix/nginx-module.nix;
+    };
+}
---
+++ nix/nginx-module.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  pkgs,
+  lib,
+  domain ? "pilum-murialis.xyz",
+  ...
+}:
+
+let
+  sitePackage = pkgs.callPackage ../site/out { };
+in
+{
+  options = {
+    services.website.enable = lib.mkEnableOption "Enable hosting the website";
+  };
+
+  config = lib.mkIf config.services.website.enable {
+    services.nginx = {
+      enable = true;
+      virtualHosts.${domain} = {
+        root = "${sitePackage}/out";
+        serverAliases = [
+          domain
+          "www.${domain}"
+        ];
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 80;
+          }
+        ];
+        locations."/" = {
+          # extraConfig = "index index.html;";
+          index = "index.html";
+          tryFiles = "$uri $uri/ =404";
+        };
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ 80 ];
+  };
+}
